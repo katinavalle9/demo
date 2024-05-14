3ad1839bf8853597a495bdfbb03de0d3150d8c97
katinavalle9 <katinavalle9@gmail.com>
2024-05-13 22:58:22 -0400

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header"
+import Saludo from "./components/Saludo"
+import Cards from "./components/Cards"
+
+function App() {
+  
+
+  return (
+    <>
+      <Header/>
+      <Saludo nombre={'Katia'}/>
+      <Cards/>
+    </>
+  )
+}
+
+export default App
